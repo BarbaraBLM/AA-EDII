4bea40563d664fdee37c49ce7915a5eaf9392a96
NathanMarts <54146961+NathanMarts@users.noreply.github.com>
2019-11-10 19:29:06 -0300

    Update hash.h

--- hash.h
+++ hash.h
@@ -1,10 +1,17 @@
+#ifndef HASH_H
+#define HASH_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "dependente.h"
-//#include "empregado.h"
+#include "empregado.h"
 
 void initHash(FILE *h, int tam);
 int hash(int n, int tam, int l);
+void imprimeHash(FILE *hash);
 void inserirHash(FILE *h, FILE *r, FILE *exclusao, Empregado *emp, int tam, int l, int *qtd_registros);
-void expandHash(FILE *h, FILE *r, int p, int tam, int l);
-int busca_por_cod(FILE *hash, FILE* regts, int cod, int tam, int l);
+void expandHash(FILE *h, FILE *r, int tam, int* p, int l);
+int buscarCod(FILE *h, FILE* regts, int cod, int tam, int p, int l);
+void excluirHash(FILE *h, FILE *r, FILE *exclusao, int end, int tam, int p, int l);
+
+#endif
