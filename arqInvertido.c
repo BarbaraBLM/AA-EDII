ab9ddc1f7e31aa5f4277cfe4baa9bfb362c57c4f
frankoliveira <57226425+frankoliveira@users.noreply.github.com>
2019-11-14 02:44:32 -0300

    Updade de arqinvertido.c
    Possui algumas correções.

--- arqInvertido.c
+++ arqInvertido.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-//#include "dependente.c"
-//#include "empregado.h"
+#include "dependente.c"
+#include "empregado.c"
 
 typedef struct invertido{
     int ED;
@@ -18,23 +18,11 @@ typedef struct invertido{
     int QTD;
 }Invertido;
 
-int cmp_nome(const void * a, const void * b){
-
-	Invertido **a1 = (Invertido **) a;
-	Invertido **b1 = (Invertido **) b;
-	if(strcmp((*a1)->emp->nome, (*b1)->emp->nome) < 0)
-		return -1;
-	else if(strcmp((*a1)->emp->nome, (*b1)->emp->nome) > 0)
-		return 1;
-	else
-		return 0;
-}
-
 Invertido* criaInvertido(){
 	Invertido *novo = (Invertido *)malloc(sizeof(Invertido));
 	novo->prox_nome = -1;
 	novo->prox_idade = -1;
-	novo->prox_salario = 1;
+	novo->prox_salario = -1;
 	novo->prox_n_dependentes = -1;
 	novo->PT = -1;
 	novo->QTD = 1;
@@ -191,11 +179,13 @@ FILE** arquivo_invert(FILE* dados, int qtd_registros){
 	ret[3] = fopen("A5-Salario.dat","w+b");
 	ret[4] = fopen("A5-Ndependts.dat","w+b");
 
+	int ant, j;
+
 	Invertido* regs[qtd_registros];
 
 	rewind(dados);//registros
 
-	//printf("Registros lidos...\n");
+	printf("Registros lidos...\n");
 	for (int i = 0; i < qtd_registros; ++i){ //lê todos os dados para a memoria principal
 		regs[i] = criaInvertido();
 		regs[i]->ED = i;
@@ -203,154 +193,147 @@ FILE** arquivo_invert(FILE* dados, int qtd_registros){
 	}
 	
 	//NOME 1-----------------------------------------
-	qsort_nome(regs, qtd_registros); //Ordena por idade
+	qsort_nome(regs, qtd_registros); //Ordena por nome
 
-	for (int i = 0; i < qtd_registros; ++i){ //Organiza os ponteiros e cria o A5-Nome
-		if(i < (qtd_registros-1) && strcmp(regs[i]->emp->nome, regs[i+1]->emp->nome) == 0 && regs[i]->emp->status == 1 && regs[i+1]->emp->status == 1){
-			for (int j = i+1; j < qtd_registros; ++j){
-				if(strcmp(regs[i]->emp->nome, regs[j]->emp->nome) == 0 && regs[i]->emp->status == 1 && regs[j]->emp->status == 1){
-					regs[i]->prox_nome = regs[j]->ED;
+	for(int i=0; i<qtd_registros; i++){
+
+		if(i < qtd_registros-1 && regs[i]->emp->status == 1){
+			ant=i;
+			j=i+1;
+			while(j<qtd_registros && strcmp(regs[i]->emp->nome, regs[j]->emp->nome) == 0){
+				if(regs[ant]->emp->status==1 && regs[j]->emp->status==1){
+					regs[ant]->prox_nome = regs[j]->ED;
 					regs[i]->QTD += 1;
+					ant = j;
 				}
 				else{
-					i = j;
+					i=j;
 					break;
 				}
+				j++;
 			}
-		}
-		if(regs[i]->emp->status == 1){
-			fwrite(regs[i]->emp->nome, sizeof(char), sizeof(regs[i]->emp->nome), ret[1]);
+			fwrite(&regs[i]->emp->nome, sizeof(char), sizeof(regs[i]->emp->nome), ret[1]);
 			fwrite(&regs[i]->ED, sizeof(int), 1, ret[1]);
 			fwrite(&regs[i]->QTD, sizeof(int), 1, ret[1]);
+			regs[i]->QTD = 1;
+			i=j;
+		}
+		if(i<qtd_registros){
+			if(regs[i]->emp->status==1){
+				fwrite(&regs[i]->emp->nome, sizeof(char), sizeof(regs[i]->emp->nome), ret[1]);
+				fwrite(&regs[i]->ED, sizeof(int), 1, ret[1]);
+				fwrite(&regs[i]->QTD, sizeof(int), 1, ret[1]);
+				regs[i]->QTD = 1;
+			}
 		}
 	}
-
-	rewind(ret[1]);//A5-idade
 	
-	//PARA VERIFICAR RESULTADO
-	char nome[50];
-	int idade, ed, qtd, n_dependentes;
-
-	//printf("A5-NOME\n");
-    while(fread(&nome, sizeof(char), sizeof(char)*50, ret[1]) != 0){
-
-        fread(&ed, sizeof(int), 1, ret[1]);
-        fread(&qtd, sizeof(int), 1, ret[1]);
-        printf("NOME:%s ED:%d Q:%d\n", nome, ed, qtd);
-    }
-    printf("\n\n");
-    
 	//IDADE 2----------------------------------------
 	qsort_idade(regs, qtd_registros); //Ordena por idade
 
-	for (int i = 0; i < qtd_registros; ++i){ //Organiza os ponteiros e cria o A5-idade
-		if(i < (qtd_registros-1) && regs[i]->emp->idade == regs[i+1]->emp->idade && regs[i]->emp->status == 1 && regs[i+1]->emp->status == 1){
-			for (int j = i+1; j < qtd_registros; ++j){
-				if(regs[i]->emp->idade == regs[j]->emp->idade && regs[i]->emp->status == 1 && regs[j]->emp->status == 1){
-					regs[i]->prox_idade = regs[j]->ED;
+	for(int i=0; i<qtd_registros; i++){
+		if(i < qtd_registros-1 && regs[i]->emp->status == 1){
+			ant=i;
+			j=i+1;
+			while(j<qtd_registros && regs[i]->emp->idade == regs[j]->emp->idade){
+				if(regs[ant]->emp->status==1 && regs[j]->emp->status==1){
+					regs[ant]->prox_idade = regs[j]->ED;
 					regs[i]->QTD += 1;
+					ant = j;
 				}
 				else{
-					if(regs[i]->emp->status==1){
-						fwrite(&regs[i]->emp->idade, sizeof(int), 1, ret[2]);
-						fwrite(&regs[i]->ED, sizeof(int), 1, ret[2]);
-						fwrite(&regs[i]->QTD, sizeof(int), 1, ret[2]);
-					}
-					i = j;
+					i=j;
 					break;
 				}
+				j++;
 			}
-		}
-		if(regs[i]->emp->status==1){
 			fwrite(&regs[i]->emp->idade, sizeof(int), 1, ret[2]);
 			fwrite(&regs[i]->ED, sizeof(int), 1, ret[2]);
 			fwrite(&regs[i]->QTD, sizeof(int), 1, ret[2]);
+			regs[i]->QTD = 1;
+			i=j;
+		}
+
+		if(i<qtd_registros){
+			if(regs[i]->emp->status==1){
+				fwrite(&regs[i]->emp->idade, sizeof(int), 1, ret[2]);
+				fwrite(&regs[i]->ED, sizeof(int), 1, ret[2]);
+				fwrite(&regs[i]->QTD, sizeof(int), 1, ret[2]);
+				regs[i]->QTD = 1;
+			}
 		}
 	}
 
-	rewind(ret[2]);//A5-idade
 
-	//PARA VERIFICAR RESULTADO
-	printf("A5-IDADE\n");
-    while(fread(&idade, sizeof(int), 1, ret[2]) != 0){
-
-        fread(&ed, sizeof(int), 1, ret[2]);
-        fread(&qtd, sizeof(int), 1, ret[2]);
-        printf("IDADE:%d ED:%d Q:%d\n", idade, ed, qtd);
-    }
-    printf("\n\n");
-
-    //SALARIOOOOOOOOO 3----------------------------------
+    //SALARIO 3----------------------------------
     qsort_salario(regs, qtd_registros); //Ordena por Salario
 
-	for (int i = 0; i < qtd_registros; ++i){ //Organiza os ponteiros e cria o A5-Salario
-		if(i < (qtd_registros-1) && regs[i]->emp->salario == regs[i+1]->emp->salario && regs[i]->emp->status == 1 && regs[i+1]->emp->status == 1){
-			for (int j = i+1; j < qtd_registros; ++j){
-				if(regs[i]->emp->salario == regs[j]->emp->salario && regs[i]->emp->status == 1 && regs[j]->emp->status == 1){
-					regs[i]->prox_salario = regs[j]->ED;
+	for(int i=0; i<qtd_registros; i++){
+		if(i < qtd_registros-1 && regs[i]->emp->status == 1){
+			ant=i;
+			j=i+1;
+			while(j<qtd_registros && regs[i]->emp->salario == regs[j]->emp->salario){
+				if(regs[ant]->emp->status==1 && regs[j]->emp->status==1){
+					regs[ant]->prox_salario = regs[j]->ED;
 					regs[i]->QTD += 1;
+					ant = j;
 				}
 				else{
-					i = j;
+					i=j;
 					break;
 				}
+				j++;
 			}
-		}
-		if(regs[i]->emp->status==1){
 			fwrite(&regs[i]->emp->salario, sizeof(double), 1, ret[3]);
 			fwrite(&regs[i]->ED, sizeof(int), 1, ret[3]);
 			fwrite(&regs[i]->QTD, sizeof(int), 1, ret[3]);
+			regs[i]->QTD = 1;
+			i=j;
+		}
+		if(i<qtd_registros){
+			if(regs[i]->emp->status==1){
+				fwrite(&regs[i]->emp->salario, sizeof(double), 1, ret[3]);
+				fwrite(&regs[i]->ED, sizeof(int), 1, ret[3]);
+				fwrite(&regs[i]->QTD, sizeof(int), 1, ret[3]);
+				regs[i]->QTD = 1;
+			}
 		}
 	}
-
-	rewind(ret[3]);//A5-salario
-
-	//PARA VERIFICAR RESULTADO
-	double salario;
-	//printf("A5-SALARIO\n");
-    while(fread(&salario, sizeof(double), 1, ret[3]) != 0){
-
-        fread(&ed, sizeof(int), 1, ret[3]);
-        fread(&qtd, sizeof(int), 1, ret[3]);
-        printf("SALARIO:%lf ED:%d Q:%d\n", salario, ed, qtd);
-    }
-    printf("\n\n");
 
     //DEPENDENTES 4-----------------------------------
     qsort_ndependentes(regs, qtd_registros); //Organiza por dependentes
 
-	for (int i = 0; i < qtd_registros; ++i){ //Organiza os ponteiros e cria o A5-Ndependents
-		if(i < (qtd_registros-1) && regs[i]->emp->n_dependentes == regs[i+1]->emp->n_dependentes && regs[i]->emp->status == 1 && regs[i+1]->emp->status == 1){
-			for (int j = i+1; j < qtd_registros; ++j){
-				if(regs[i]->emp->n_dependentes == regs[j]->emp->n_dependentes && regs[i]->emp->status == 1 && regs[j]->emp->status == 1){
-					regs[i]->prox_n_dependentes = regs[j]->ED;
+	for(int i=0; i<qtd_registros; i++){
+		if(i < qtd_registros-1 && regs[i]->emp->status == 1){
+			ant=i;
+			j=i+1;
+			while(j<qtd_registros && regs[i]->emp->n_dependentes == regs[j]->emp->n_dependentes){
+				if(regs[ant]->emp->status==1 && regs[j]->emp->status==1){
+					regs[ant]->prox_n_dependentes = regs[j]->ED;
 					regs[i]->QTD += 1;
+					ant = j;
 				}
-				else {
-					i = j;
+				else{
+					i=j;
 					break;
 				}
+				j++;
 			}
-		}
-		if(regs[i]->emp->status==1){
 			fwrite(&regs[i]->emp->n_dependentes, sizeof(int), 1, ret[4]);
 			fwrite(&regs[i]->ED, sizeof(int), 1, ret[4]);
 			fwrite(&regs[i]->QTD, sizeof(int), 1, ret[4]);
+			regs[i]->QTD = 1;
+			i=j;
+		}
+		if(i<qtd_registros){
+			if(regs[i]->emp->status==1){
+				fwrite(&regs[i]->emp->n_dependentes, sizeof(int), 1, ret[4]);
+				fwrite(&regs[i]->ED, sizeof(int), 1, ret[4]);
+				fwrite(&regs[i]->QTD, sizeof(int), 1, ret[4]);
+				regs[i]->QTD = 1;
+			}
 		}
 	}
-
-	rewind(ret[3]);//A5-salario
-
-	//PARA VERIFICAR RESULTADO
-	//printf("A5-Ndependts\n");
-    while(fread(&n_dependentes, sizeof(int), 1, ret[3]) != 0){
-
-        fread(&ed, sizeof(int), 1, ret[3]);
-        fread(&qtd, sizeof(int), 1, ret[3]);
-        printf("NDEPENDENTS:%d ED:%d Q:%d\n", n_dependentes, ed, qtd);
-    }
-    printf("\n\n");
-
 
 	qsort_cp(regs, qtd_registros);
 	
@@ -362,7 +345,7 @@ FILE** arquivo_invert(FILE* dados, int qtd_registros){
 		fwrite(&regs[i]->emp->salario, sizeof(double), 1, ret[0]);
 		fwrite(&regs[i]->emp->n_dependentes, sizeof(int), 1, ret[0]);
 		fwrite(&regs[i]->prox_nome, sizeof(int), 1, ret[0]);//ponteiro p/ prox nome
-		fwrite(&regs[i]->prox_idade, sizeof(int), 1, ret[0]);//ponteiro p/ prox idade
+		fwrite(&regs[i]->prox_idade, sizeof(int), 1, ret[0]);//ponteiro p/ prox idad		
 		fwrite(&regs[i]->prox_salario, sizeof(int), 1, ret[0]);//ponteiro p/ prox salario
 		fwrite(&regs[i]->prox_n_dependentes, sizeof(int), 1, ret[0]);//ponteiro p/ prox n_dependentes
 	}
