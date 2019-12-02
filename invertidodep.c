58c9e6f7f5a46980c6cf37dbfe95db1f51b83692
BarbaraBLM <47249285+BarbaraBLM@users.noreply.github.com>
2019-12-02 14:16:35 -0300

    Update invertidodep.c
    Tirando status de A8

--- invertidodep.c
+++ invertidodep.c
@@ -207,7 +207,6 @@ FILE** arquivo_invertido_dep(FILE* dados, int qtd_registros){
 		fwrite(&regs[i]->dep->idade, sizeof(int), 1, ret[0]);
 		fwrite(&regs[i]->prox_nome, sizeof(int), 1, ret[0]);//ponteiro p/ prox nome
 		fwrite(&regs[i]->prox_idade, sizeof(int), 1, ret[0]);//ponteiro p/ prox idad	
-		fwrite(&regs[i]->status, sizeof(int), 1, ret[0]);
 	}
 
     return ret;
